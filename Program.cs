95e97b67740b2c46ff0d0bdbf0be5997f384754a
Brayan17king <104658332+Brayan17king@users.noreply.github.com>
2023-09-15 10:35:39 -0500

    feat: :memo: Main Program C#

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using LinQ_CSharp;
+using LinQ_CSharp.Entities;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        LinqQueries queries = new LinqQueries();
+        // ImprimirValores(queries.AllCollection());
+        // ImprimirValores(queries.LibrosDespues2000());
+        //ImprimirValores(queries.LibrosAndroid());
+        //ImprimirValores(queries.LibrosAnd2005());
+        // ImprimirValores(queries.LibrosAct250());
+        // Console.WriteLine(queries.BookAllStatus() ? "Todos los Libros contienen el Status" : "Almenos uno de los Libros no Contiene el status");
+        //ImprimirValores(queries.BookAnyStatus());
+        ImprimirValores(queries.ContainsStatuts());
+    }
+
+    private static void ImprimirValores(IEnumerable<Book> books)
+    {
+        if (books != null)
+        {
+            int registros = 0;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0, -70} {1, 7} {2, 20}", "Titulo", "Nro Paginas", "Fecha Publicación");
+            foreach (Book book in books)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                registros += 1;
+                Console.WriteLine("{0, -70} {1, 7} {2, 20}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("No hay ningun libro con esas condiciones");
+        }
+    Console.ForegroundColor = ConsoleColor.Gray;
+    }
+}
+ 
